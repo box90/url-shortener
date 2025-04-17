d8d385a5031208ff7e025ad551775eb7276ce146
marco.bossi <marco.bossi90@gmail.com>
2025-04-17 17:55:07 +0200

    feat: use motor as async client for mongo

--- src/infrastructure/mongodb/mongodb_url_repo_impl.py
+++ src/infrastructure/mongodb/mongodb_url_repo_impl.py
@@ -1,44 +1,30 @@
-from typing import List
-
-from pymongo import MongoClient, ASCENDING
-from pymongo.errors import DuplicateKeyError
+from pymongo import ASCENDING
+from motor.motor_asyncio import AsyncIOMotorClient
 
 from configs import MONGO_URI, DB_NAME, COLLECTION_NAME
 from domain.models.short_url import ShortURL
 from domain.repositories.url_repository import URLRepository
-from infrastructure.mongodb.converter import doc_to_model, model_to_doc
-
-client = MongoClient(MONGO_URI)
-db = client[DB_NAME]
-collection = db[COLLECTION_NAME]
-
-# Create index for uniqueness
-collection.create_index("original_url", unique=True)
-
-# Create index for expiration
-collection.create_index([("expire_at", ASCENDING)], expireAfterSeconds=0)
 
 
 class MongoURLRepository(URLRepository):
-    async def get_by_original(self, url: str) -> ShortURL | None:
-        doc = collection.find_one({"original_url": url})
-        if doc:
-            return doc_to_model(doc)
-        return None
+    def __init__(self):
+        self._client = AsyncIOMotorClient(MONGO_URI)
+        self._db = self._client[DB_NAME]
+        self._collection = self._db[COLLECTION_NAME]
+        self._collection.create_index([("expire_at",ASCENDING)], expireAfterSeconds=0)
+        self._collection.create_index("original_url", unique=True)
 
-    async def get_by_code(self, code: str) -> ShortURL | None:
-        doc = collection.find_one({"short_code": code})
-        if doc:
-            return doc_to_model(doc)
-        return None
+    async def get_by_original(self, url: str):
+        doc = await self._collection.find_one({"original_url": url})
+        return ShortURL(**doc) if doc else None
 
-    async def save(self, short_url: ShortURL) -> None:
-        try:
-            doc = model_to_doc(short_url)
-            collection.insert_one(doc)
-        except DuplicateKeyError:
-            raise ValueError(f"Url {short_url.original_url} already exists.")
+    async def get_by_code(self, code: str):
+        doc = await self._collection.find_one({"short_code": code})
+        return ShortURL(**doc) if doc else None
 
-    async def list_all(self) -> List[ShortURL]:
-        docs = collection.find({}, {"_id": 0})
-        return [doc_to_model(d) for d in docs]
+    async def save(self, short_url: ShortURL):
+        await self._collection.insert_one(short_url.__dict__)
+
+    async def list_all(self):
+        cursor = self._collection.find({})
+        return [ShortURL(**doc) async for doc in cursor]
